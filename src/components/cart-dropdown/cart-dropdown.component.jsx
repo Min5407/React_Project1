8c2ed62ec46bc4a4147ebf780cade740e2a2c2dd
Min <min@Mins-MacBook-Pro.local>
2020-05-12 16:01:55 +0900

    cart item component added which is used in the cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./cart-dropdown.style.scss";
 import { Button } from "../button/button.component";
+import CartItem from "../cart-item/cartItem.component";
 
-const CartDropDown = () => (
+import { connect } from "react-redux";
+
+const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <Button> Check Out</Button>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropDown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,14 +3,16 @@ export const AddItemToCart = (cartItems, cartItemToAdd) => {
     (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
-  //findexistingItem = true if can find from the previous cart items list
+  //findexistingItem will become true if the item that has been added can be found in the previous cart items list
   if (findExistingItem) {
+    // this function is used to increase a quatity in the cartItem
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
+  } else {
+    //   the code below will create quatity property in the object since there is not quantity property in the beginning
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
   }
-
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
